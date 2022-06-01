593c0fc65817cab19c07efe1b596016694e83190
Avichal <pandeyavichal7@gmail.com>
2022-06-01 22:02:36 +0530

    Deploy on Fuji testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+const FUJI_PRIVATE_KEY = process.env.FUJI_PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +20,22 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.4',
+  solidity: '0.8.4',    
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",      
+    },
+    fujiAvalanche: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43113,    
+      accounts: [`0x${FUJI_PRIVATE_KEY}`],    
+    },
+    // TODO: change it to FUJI
+    hardhat: {
+      forking: {
+        url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      },
+    },
+  }
 };
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
+  const AvaxPool = await hre.ethers.getContractFactory('AvaxPool');
+  const avaxPool = await AvaxPool.deploy();
 
-  await greeter.deployed();
+  await avaxPool.deployed();
 
-  console.log('Greeter deployed to:', greeter.address);
+  console.log('AvaxPool deployed to:', avaxPool.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
