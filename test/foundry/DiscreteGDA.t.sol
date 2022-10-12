a49e081648256b8e811cc6da6dbb5689ff175ad3
avichalp <hi@avichalp.me>
2022-10-12 21:15:07 +0800

    Add Discreet Gradual Dutch Auction implementation

---
+++ test/foundry/DiscreteGDA.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../../src/DiscreteGDA.sol";
+
+contract MockDiscreteGDA is DiscreteGDA {
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    int256 _initialPrice,
+    int256 _scalerFactor,
+    int256 _decayConstant
+  ) DiscreteGDA(_name, _symbol, _initialPrice, _scalerFactor, _decayConstant) {}
+
+  function tokenURI(uint256)
+    public
+    pure
+    virtual
+    override
+    returns (string memory)
+  {}
+}
+
+contract DiscreteGDATest is Test {
+  using PRBMathSD59x18 for int256;
+  //using Strings for uint256;
+
+  DiscreteGDA public gda;
+
+  int256 public initialPrice = PRBMathSD59x18.fromInt(1000);
+  // lambda = 1/2
+  int256 public decayConstant =
+    PRBMathSD59x18.fromInt(1).div(PRBMathSD59x18.fromInt(2));
+  // alpha = 1.1
+  int256 public scaleFactor =
+    PRBMathSD59x18.fromInt(11).div(PRBMathSD59x18.fromInt(10));
+
+  bytes insufficientPayment = abi.encodeWithSignature("InsufficientPayment()");
+
+  function setUp() public {
+    gda = new MockDiscreteGDA(
+      "Token",
+      "TKN",
+      initialPrice,
+      scaleFactor,
+      decayConstant
+    );
+  }
+
+  function testInitialPrice() public {
+    uint256 initial = uint256(initialPrice);
+    uint256 quantity = 1;
+    uint256 purchasePrice = gda.purchasePrice(quantity);
+
+    // initially the purchace price should be same (within 1/10 of percent)
+    // as the calculated purchase price
+    assertApproxEqRel(purchasePrice, initial, 1_000000_000000000);
+  }
+
+  function testInsufficientPayment() public {
+    uint256 quantity = 1;
+    uint256 purchasePrice = gda.purchasePrice(quantity);
+    vm.deal(address(this), purchasePrice);
+    vm.expectRevert(insufficientPayment);
+    // try to buy `quantity` amount of tokens for 1 wei less than the price
+    gda.purchaseTokens{ value: purchasePrice - 1 }(quantity, address(this));
+  }
+
+  function testMintCorrectly() public {
+    // assertTrue(gda.ownerOf(1) != address(this));
+    uint256 quantity = 1;
+    uint256 purchasePrice = gda.purchasePrice(quantity);
+    vm.deal(address(this), purchasePrice);
+    // use purchase price to buy the tokens
+    gda.purchaseTokens{ value: purchasePrice }(quantity, address(this));
+    assertTrue(gda.ownerOf(1) == address(this));
+  }
+
+  function testRefund() public {
+    uint256 quantity = 1;
+    uint256 purchasePrice = gda.purchasePrice(quantity);
+    vm.deal(address(this), 2 * purchasePrice);
+    // pay twice, should get back the amount
+    gda.purchaseTokens{ value: 2 * purchasePrice }(quantity, address(this));
+    assertTrue(address(this).balance == purchasePrice);
+  }
+
+  receive() external payable {}
+}
